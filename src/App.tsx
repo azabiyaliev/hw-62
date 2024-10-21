6505a54f55c813bf9c394b16edb124657bd0372b
azabiyaliev <aza.biyaliev@gmail.com>
2024-10-21 16:02:43 +0600

    Отрисовал Header с 2-мя ссылками. Реализовал переход от одной страницы на другую.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import Home from './components/Home/Home.tsx';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import AboutUs from './components/AboutUs/AboutUs.tsx';
+import Navbar from './components/Navbar/Navbar.tsx';
 
 const App = () => {
   return (
     <>
-
+      <Navbar/>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/AboutUs" element={<AboutUs/>}></Route>
+        </Routes>
+      </div>
     </>
   );
 };
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="nav bg-secondary">
+      <div className="container">
+        <div className="w-100 row row-cols-2 justify-content-between align-items-center">
+          <div><span className="navbar-brand mb-0 text-white fs-1">B r a i n S t o r m </span></div>
+        </div>
+        <div className="ms-auto">
+          <nav className="navbar navbar-nav row row-cols-1">
+            <li>
+              <NavLink className="text-white text-decoration-none" to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink className="text-white text-decoration-none" to="/AboutUs">About us</NavLink>
+            </li>
+          </nav>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
